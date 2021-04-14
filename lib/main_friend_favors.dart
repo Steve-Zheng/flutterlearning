aabe91dc5ad8b913592c2e14e1213e2676dbf03d
Zheng Qichun <zqc111123@gmail.com>
2021-04-14 21:27:44 +0800

    Minor changes

--- lib/main_friend_favors.dart
+++ lib/main_friend_favors.dart
@@ -1,18 +1,15 @@
-import 'dart:io';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterlearning/pages/login_page.dart';
 import 'package:flutterlearning/theme.dart';
 
-void main()async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter Demo",
@@ -22,4 +19,3 @@ class MyApp extends StatelessWidget{
     );
   }
 }
-
